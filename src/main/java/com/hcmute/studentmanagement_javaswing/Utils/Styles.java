9a1dfb88c9ecc32529fcceb64939757f31ccc51a
lamtanng <laam122003@gmail.com>
2024-03-12 14:11:14 +0700

    feat/SCHED-2: Create Login form; add Styles.java common file

---
+++ src/main/java/com/hcmute/studentmanagement_javaswing/Utils/Styles.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hcmute.studentmanagement_javaswing.Utils;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Styles {
+    //Color
+    public static final Color COLOR_PRIMARY = new java.awt.Color(0, 153, 255);
+    public static final Color COLOR_BLACK = new java.awt.Color(0, 0, 0);
+    public static final Color COLOR_WHITE = new java.awt.Color(255,255,255);
+
+    
+    //Font
+    public static final Font TEXT_BOLD_MEDIUM = new java.awt.Font("Roboto", 1, 16);
+    public static final Font TEXT_PLAIN_MEDIUM = new java.awt.Font("Roboto", 0, 16);
+}
