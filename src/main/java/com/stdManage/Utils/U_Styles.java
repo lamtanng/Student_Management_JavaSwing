3b19fe1d72e71990cebb399ae5ac2aca307c0bfe
lamtanng <laam122003@gmail.com>
2024-03-23 11:57:53 +0700

    feat/SCHED-13: create Utils class

--- src/main/java/com/stdManage/Utils/U_Styles.java
+++ src/main/java/com/stdManage/Utils/U_Styles.java
@@ -11,13 +11,20 @@ import java.awt.Font;
  *
  * @author ADMIN
  */
-public class Styles {
+public class U_Styles {
     //Color
     public static final Color COLOR_PRIMARY = new java.awt.Color(0, 153, 255);
     public static final Color COLOR_BLACK = new java.awt.Color(51, 51, 51);
-    public static final Color COLOR_GRAY1 = new java.awt.Color(230, 230, 230);
+    public static final Color COLOR_GRAY1 = new java.awt.Color(250, 250, 250);
+    public static final Color COLOR_GRAY2 = new java.awt.Color(230, 230, 230);
+    public static final Color COLOR_GRAY3 = new java.awt.Color(179, 179, 179);
+    public static final Color COLOR_GRAY4 = new java.awt.Color(115, 115, 115);
     public static final Color COLOR_WHITE = new java.awt.Color(255,255,255);
-
+    
+    
+    //Size
+    public static final int HEIGHT_ROW = 40;
+    public static final int WIDHT_ROW = 60;
     
     //Font
     public static final Font TEXT_BOLD_MEDIUM = new java.awt.Font("Roboto", 1, 14);
