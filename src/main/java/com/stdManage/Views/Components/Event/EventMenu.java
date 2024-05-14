1c1cad427be7de89d1b45ea24504b6b0fb0d5629
khiemld <khiemld.0204@gmail.com>
2024-05-14 23:38:13 +0700

    Refactor Project

--- src/main/java/com/stdManage/Views/Components/Event/EventMenu.java
+++
@@ -1,8 +0,0 @@
-package com.stdManage.Views.Components.Event;
-
-import java.awt.Component;
-
-public interface EventMenu {
-
-    public boolean menuPressed(Component com, boolean open);
-}
--- src/main/java/com/stdManage/Views/Components/Event/EventMenuSelected.java
+++
@@ -1,6 +0,0 @@
-package com.stdManage.Views.Components.Event;
-
-public interface EventMenuSelected {
-
-    public void menuSelected(int menuIndex, int subMenuIndex);
-}
