d13401e3d1b264b2a57bc0bdbc8fed66b1b1d451
BlackDang <hoangvutruonggiang28@gmail.com>
2024-05-15 22:54:54 +0700

    build: add ultils

---
+++ src/Ultils/Constant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ultils;
+
+/**
+ *
+ * @author Trường Giang
+ */
+public final class Constant {
+
+    public static final String PHONE_TYPE = "PHONE";
+    public static final String EMAIL_TYPE = "EMAIL";
+}
