fd977e8ff60341d3ebcfe5a7ae090ae2cc43e762
BlackDang <hoangvutruonggiang28@gmail.com>
2024-05-16 01:31:33 +0700

    fix: handle logic in SanphamController, add Ultils to show multi error item

--- src/Ultils/MyUtils.java
+++ src/Ultils/MyUtils.java
@@ -1,12 +1,39 @@
 package Ultils;
+
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class MyUtils {
-	public static void showErrorMessage(String title, String errorMsg) {
-		JOptionPane.showMessageDialog(null, errorMsg, title, JOptionPane.ERROR_MESSAGE);
-	}
-	
-	public static void showInfoMessage(String title, String errorMsg) {
-		JOptionPane.showMessageDialog(null, errorMsg, title, JOptionPane.INFORMATION_MESSAGE);
-	}
+
+    static ArrayList<String> errorList = new ArrayList<String>();
+
+    public static void showErrorMessage(String title, String errorMsg) {
+        JOptionPane.showMessageDialog(null, errorMsg, title, JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void showInfoMessage(String title, String errorMsg) {
+        JOptionPane.showMessageDialog(null, errorMsg, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void addErrorMessage(String title) {
+        errorList.add(title);
+    }
+
+    public static void showErrorList() {
+        String errorStr = "";
+        for (String string : errorList) {
+            errorStr = errorStr.concat(string + "\n");
+        }
+        JOptionPane.showMessageDialog(null, errorStr, "Error", JOptionPane.ERROR_MESSAGE);
+        clearErrorList();
+    }
+
+    public static void clearErrorList() {
+        errorList = new ArrayList<String>();
+    }
+
+    public static boolean isErrorListEmpty() {
+        return errorList.isEmpty();
+    }
+
 }
